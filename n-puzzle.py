efbbe52c694123daa94ca4e6997a0e24ad139c86
Kai Bedene <kbedene@z4r4p5.le-101.fr>
2020-01-10 13:23:26 +0100

    inputs stored in nested array, inut error checking inc

--- n-puzzle.py
+++ n-puzzle.py
@@ -31,6 +31,28 @@ def filterCommentFromLine(line):
         line = line[0:commentIndex]
     return line.strip()
 
+def filterEmptyString(array):
+    def test(e):
+        return e != ""
+    return list(filter(test, array))
+
+def getPuzzle(puzzleSize):
+    puzzle = [[-1 for i in range(0, puzzleSize)] for j in range(0, puzzleSize)]
+    for y, line in enumerate(sys.stdin):
+        line = filterCommentFromLine(line)
+        lineSplitted = line.split(" ")
+        lineSplitted = filterEmptyString(lineSplitted)
+        if (len(lineSplitted) != puzzleSize):
+            print("ERROR: Every row must strictly be of the puzzle size.")
+            sys.exit()
+        for x in range(0, puzzleSize):
+            try:
+                puzzle[y][x] = int(lineSplitted[x])
+            except ValueError:
+                print("ERROR: Each lines must be composed of unique integers ranging from 0 to [puzzle size]^2 - 1.")
+                sys.exit()
+    return puzzle
+
 def getPuzzleSize():
     puzzleSize = 0
     for line in sys.stdin:
@@ -39,13 +61,15 @@ def getPuzzleSize():
             continue
         lineSplit = line.split(" ")
         if (len(lineSplit) != 1 or not helpers.str_isInt(lineSplit[0])):
-            helpers.exit("The first line must be single int representing the puzzle size.")
+            print("ERROR: The first line must be single int representing the puzzle size.")
+            sys.exit()
         puzzleSize = int(lineSplit[0])
-        if (puzzleSize < 3):
-            helpers.exit("The puzzle size must be at least of 3")
+        if (puzzleSize < 2):
+            print("ERROR: The puzzle size must be at least of 2")
+            sys.exit()
         break
     if (puzzleSize == None):
-        print("You must enter a puzzle size.")
+        print("ERROR: You must enter a puzzle size.")
     return puzzleSize
 
 if __name__ == "__main__":
